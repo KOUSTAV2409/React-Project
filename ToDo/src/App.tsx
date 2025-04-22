a1273fe2871722451089919d5eb3a034dfa2e9b0
Koustav Ganguly <koustavganguly24@gmail.com>
2025-04-22 12:44:42 +0530

    fetched data from api, displayed the data in multiple card , installed and configured shadcn ui within the project

--- ToDo/src/App.tsx
+++ ToDo/src/App.tsx
@@ -1,14 +1,15 @@
 // import { useState } from 'react'
 
 import './App.css'
-import Counter from './Components/Counter'
+import ToDo from './Components/ToDo'
+
 
 function App() {
 
   return (
     <>
 
-      <Counter />
+      <ToDo/>
 
     </>
   )
---
+++ ToDo/src/Components/ToDo.tsx
@@ -0,0 +1,61 @@
+// import React from 'react'
+import { useEffect, useState } from 'react'
+
+
+const ToDo = () => {
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+        const todo = async () => {
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+                const jsonData = await response.json()
+                setTodos(jsonData)
+
+            } catch (error) {
+                console.log(error)
+
+            }
+        }
+        todo()
+    }, [])
+
+    const Card = ({ id, title }) => {
+        return (
+            <>
+                <div className='h-40 w-44 border'>
+                    <h1 className='text-center'>Task Card</h1>
+
+                    <p>Task : {id}</p>
+                    <p>Task Title : {title}</p>
+
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div className="h-lvh w-full">
+                <h1 className='text-center border-2 m-2 p-2 rounded-md text-2xl font-bold'>To-Do Application</h1>
+
+
+                <div>
+                    <h1 className='text-center border-2 m-2 p-1 rounded-md text-2xl font-bold'>
+                        All Saved Task :
+                    </h1>
+                    <div className='flex flex-wrap justify-center gap-4'>
+                    {todos.map(({ id, title }) => {
+                        return (
+                            <Card key={id} id={id} title={title} />
+
+                        )
+                    })}
+                </div>
+                </div>
+            </div>
+        
+        </>
+    )
+}
+
+export default ToDo
