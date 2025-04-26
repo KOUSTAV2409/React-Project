92d025e7f4f258c15c6737479420b826d3bc106c
Koustav Ganguly <koustavganguly24@gmail.com>
2025-04-26 15:32:11 +0530

    refactor: remove unused React import and enhance button hover effects in Landing component

--- ToDo Full/ToDo/src/Components/Landing.tsx
+++ ToDo Full/ToDo/src/Components/Landing.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 const Landing = () => {
   return (
@@ -10,17 +10,17 @@ const Landing = () => {
 
         <div className="flex flex-col gap-6 md:flex-row justify-center items-center">
           <button
-            className="w-56 h-14 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold rounded-lg shadow-lg hover:bg-gradient-to-l focus:outline-none transition duration-300 transform hover:scale-105"
+            className="w-56 h-14 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold rounded-lg shadow-lg hover:bg-gradient-to-l focus:outline-none transition duration-300 transform hover:scale-105 hover:cursor-pointer"
           >
             Create To-Do
           </button>
           <button
-            className="w-56 h-14 bg-gradient-to-r from-teal-500 to-teal-700 text-white font-semibold rounded-lg shadow-lg hover:bg-gradient-to-l focus:outline-none transition duration-300 transform hover:scale-105"
+            className="w-56 h-14 bg-gradient-to-r from-teal-500 to-teal-700 text-white font-semibold rounded-lg shadow-lg hover:bg-gradient-to-l focus:outline-none transition duration-300 transform hover:scale-105 hover:cursor-pointer"
           >
             Recent To-Do
           </button>
           <button
-            className="w-56 h-14 bg-gradient-to-r from-gray-500 to-gray-700 text-white font-semibold rounded-lg shadow-lg hover:bg-gradient-to-l focus:outline-none transition duration-300 transform hover:scale-105"
+            className="w-56 h-14 bg-gradient-to-r from-gray-500 to-gray-700 text-white font-semibold rounded-lg shadow-lg hover:bg-gradient-to-l focus:outline-none transition duration-300 transform hover:scale-105 hover:cursor-pointer"
           >
             Old To-Do
           </button>
