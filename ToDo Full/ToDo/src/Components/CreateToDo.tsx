add5f121e01e630e092902fe20072ba7922f436a
Koustav Ganguly <koustavganguly24@gmail.com>
2025-04-26 11:42:01 +0530

    minor changes

--- ToDo Full/ToDo/src/Components/CreateToDo.tsx
+++ ToDo Full/ToDo/src/Components/CreateToDo.tsx
@@ -1,21 +1,42 @@
-import { useState } from "react"
+import React, { useState } from 'react'
 
- 
- 
- const CreateToDo = () => {
+const CreateToDo = () => {
+  const [task, setTask] = useState('')
+  const [addButton, setAddButton] = useState([])
 
-  const [task,setTask]=useState('')
-   return (
-     <div>
-      <h1 className="text-center border border-black rounded-md text-2xl font-semibold">your ToDo</h1>
+  const handleClick = () => {
+    if (task.trim()) {
+      setAddButton([...addButton, task]);
+      setTask('')
+    }
+    else (alert("you have not added any task yet"))
+  }
 
+  const handleDelete = (index) => {
+    // Remove the task at the specified index
+    const updatedTasks = addButton.filter((_, i) => i !== index);
+    setAddButton(updatedTasks);
+  };
+  return (
+    <div>
+      <h1 className='text-center font-bold text-2xl border rounded-md m-2 p-2'>Your ToDo</h1>
 
-      <input type="text" placeholder="Add your task" />
-      <button>Add</button>
+      <div className='m-2 gap-4'>
+        <input type="text" placeholder='Enter your task :' value={task} onChange={(e) => { setTask(e.target.value) }} />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <ol className='m-2 list-decimal list-inside'>
+        {addButton.map((item, index) => (
+          <li key={index} className='flex justify-between items-center gap-4'>
+            <span>{item}</span>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+            <button>Update</button>
+          </li>
 
-      <p></p>
-     </div>
-   )
- }
- 
- export default CreateToDo
+        ))}
+      </ol>
+    </div>
+  )
+}
+
+export default CreateToDo
